15643f6ae341f02a0641c5f6af3fd32af51c09a9
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-02-13 04:23:53 +0800

    adding function folder

---
+++ function/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    sayHi("Mike", 40);
+    sayHi("Tom", 23);
+    sayHi("Oscar", 70);
+    return 0;
+}
+
+void sayHi(char name[], int age) {
+    printf("Hello %s, you are %d\n", name, age);
+}
