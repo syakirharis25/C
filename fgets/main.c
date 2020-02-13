241511bbc929f65e6be50ff776352c0fabe34586
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-02-14 00:14:04 +0800

    adding modified fgets folder

--- fgets/main.c
+++ fgets/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    char name[20];
-    printf("Enter your name: ");
-    fgets(name, 20, stdin);
-    printf("Your name is %s asdasd", name);
+int main(){
 
+    char line[255];
+    FILE * fpointer = fopen("employees.txt", "r");
+
+    fgets(line, 255, fpointer);
+    fgets(line, 255, fpointer);
+    printf("%s", line);
+
+    fclose(fpointer);
     return 0;
 }
